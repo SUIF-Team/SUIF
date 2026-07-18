f34f2861522580de8fb04f1649e453357b208ee9
Anuar Manuel Olvera Ramirez <anuar.manuel320@comunidad.unam.mx>
2026-07-17 19:02:59 -0600

    Reglas de negocio y datos del footer

---
+++ config/suif.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'cuota_recuperacion' => 7000.00,
+    'moneda' => 'MXN',
+    'soporte_correo' => 'soportesistemas@fca.unam.mx',
+    'enlaces' => [
+        'aviso_privacidad' => '#',
+        'documento_seguridad' => '#',
+        'instrumento_juridico' => '#',
+        'aviso_privacidad_simplificado' => '#',
+        'cifca' => '#',
+    ],
+];
