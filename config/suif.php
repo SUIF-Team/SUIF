b258680f7323a22a496410a83646a6c52929a087
Alejandro Isaías Díaz Castellanos <alexnuno218@gmail.com>
2026-08-05 14:49:08 -0600

    feat(participante): centraliza el avance del trámite en AvanceParticipante

--- config/suif.php
+++ config/suif.php
@@ -4,6 +4,22 @@ return [
     'cuota_recuperacion' => 7000.00,
     'moneda' => 'MXN',
     'soporte_correo' => 'soportesistemas@fca.unam.mx',
+        'documentos' => [
+        'solicitud-firmada' => 'Solicitud firmada',
+        'aceptacion-notificaciones' => 'Aceptación de notificaciones',
+        'carta-bajo-protesta' => 'Carta bajo protesta',
+        'autorizacion-publicacion' => 'Autorización de la publicación',
+        'curp' => 'CURP',
+        'identificacion-oficial' => 'Identificación oficial',
+    ],
+
+    /* Los que tienen formato oficial descargable. */
+    'formatos_descargables' => [
+        'solicitud-firmada',
+        'aceptacion-notificaciones',
+        'carta-bajo-protesta',
+        'autorizacion-publicacion',
+    ],
     'enlaces' => [
         'unam' => 'https://www.unam.mx/',
         'fca' => 'https://www.fca.unam.mx/',
